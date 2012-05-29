795e8ea568ab7926bfa64feb9796317bb716c2bd
joaolevada <joaolevada@gmail.com>
2012-05-28 23:39:33 -0300

    A little progress to the implementation.

--- source/core/rawmaterial/RawMaterialBO.pas
+++ source/core/rawmaterial/RawMaterialBO.pas
@@ -9,10 +9,22 @@ uses
   SysUtils,
   CustomBO,
   PressAttributes,
-  PressSubject;
+  PressSubject,
+  BasicUserRecordDataBO;
 
 type
+
+  TUnity = (uGram, uKilogram, uMilliliter, uLiter);
+
+  { TRawMaterial }
+
   TRawMaterial = class(TCustomObject)
+    _BasicUserRecordData: TBasicUserRecordDataPart;
+    _Unity: TPressEnum;
+  public
+    class function InternalMetadataStr: string; override;
+  published
+    BasicUserRecordData: TBasicUserRecordData;
   end;
 
   TRawMaterialReference = class(TCustomReference)
@@ -20,7 +32,24 @@ type
 
 implementation
 
+{ TRawMaterial }
+
+class function TRawMaterial.InternalMetadataStr: string;
+begin
+  Result := 'TRawMaterial IsPersistent (' +
+    'BasicUserRecordData: TBasicUserRecordDataPart ShortName="BasicURD";' +
+    'Unity: Enum(TUnity);' +
+    ')';
+end;
+
 initialization
+  PressModel.RegisterEnumMetadata(TypeInfo(TUnity), 'TUnity',
+    [
+    'Grama (gr)',
+    'Quilograma (kg)',
+    'Mililitro (ml)',
+    'Litro (l)'
+    ]);
   TRawMaterial.RegisterClass;
   TRawMaterialReference.RegisterAttribute;
 
