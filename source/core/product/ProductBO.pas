09fd534923572d3c9b0ee7caf4860a40e8f910c1
Joao Henrique Levada <joaolevada@gmail.com>
2013-03-05 22:57:47 -0300

    Starting a revamp on the project. Product business object and some related classes...

--- source/core/product/ProductBO.pas
+++ source/core/product/ProductBO.pas
@@ -13,6 +13,33 @@ uses
 
 type
 
+  { TBudget }
+
+  TBudget = class(TCustomObject)
+    _Code: TPressPlainString;
+    _Name: TPressAnsiString;
+    _Remarks: TPressMemo;
+    _Supplier: TPressReference;
+    _Shipping: TPressCurrency;
+    _SumOfItems: TPressCurrency;
+    _TotalBudget: TPressCurrency;
+    _Items: TPressParts;
+  protected
+    class function InternalMetadataStr: string; override;
+  end;
+
+  { TBudgetItem }
+
+  TBudgetItem = class(TCustomObject)
+    _Product: TPressReference;
+    _Unity: TPressReference;
+    _Quantity: TPressFloat;
+    _UnityValue: TPressCurrency;
+    _TotalValue: TPressCurrency;
+  protected
+    class function InternalMetadataStr: string; override;
+  end;
+
   { TProduct }
 
   TProduct = class(TCustomObject)
@@ -29,15 +56,100 @@ type
     class function InternalMetadataStr: string; override;
   end;
 
+  { TInvoice }
+
+  TInvoice = class(TCustomObject)
+    _Code: TPressPlainString;
+    _Name: TPressAnsiString;
+    _Remarks: TPressMemo;
+    _Supplier: TPressReference;
+    _Shipping: TPressCurrency;
+    _SumOfItems: TPressCurrency;
+    _TotalInvoice: TPressCurrency;
+    _Items: TPressParts;
+  protected
+    class function InternalMetadataStr: string; override;
+  end;
+
+  { TInvoiceItem }
+
+  TInvoiceItem = class(TCustomObject)
+    _Product: TPressReference;
+    _Unity: TPressReference;
+    _Quantity: TPressFloat;
+    _UnityValue: TPressCurrency;
+    _TotalValue: TPressCurrency;
+  protected
+    class function InternalMetadataStr: string; override;
+  end;
+
 implementation
+
+{ TInvoiceItem }
+
+class function TInvoiceItem.InternalMetadataStr: string;
+begin
+  Result := 'TInvoiceItem IsPersistent(' +
+    'Product: Reference(TProduct);' +
+    'Unity: Reference(TUnity);' +
+    'Quantity: Float;' +
+    'UnityValue: Currency;' +
+    'TotalValue: Currency;' +
+    ');';
+end;
+
+{ TInvoice }
+
+class function TInvoice.InternalMetadataStr: string;
+begin
+  Result := 'TInvoice IsPersistent (' +
+    'Code: PlainString(20);' +
+    'Name: AnsiString(40) DisplayName="Nome" IsMandatory=True;' +
+    'Remarks: Memo;' +
+    'Supplier: Reference(TContact);' +
+    'Shipping: Currency;' +
+    'SumOfItems: Currency Calc(Items);' +
+    'TotalInvoice: Currency Calc(SumOfItems, Shipping);' +
+    'Items: Parts(TBudgetItem);' +
+    ');';
+end;
+
+{ TBudgetItem }
+
+class function TBudgetItem.InternalMetadataStr: string;
+begin
+  Result := 'TBudgetItem IsPersistent(' +
+    'Product: Reference(TProduct);' +
+    'Unity: Reference(TUnity);' +
+    'Quantity: Float;' +
+    'UnityValue: Currency;' +
+    'TotalValue: Currency;' +
+    ');';
+end;
+
+{ TBudget }
+
+class function TBudget.InternalMetadataStr: string;
+begin
+  Result := 'TBudget IsPersistent (' +
+    'Code: PlainString(20);' +
+    'Name: AnsiString(40) DisplayName="Nome" IsMandatory=True;' +
+    'Remarks: Memo;' +
+    'Supplier: Reference(TContact);' +
+    'Shipping: Currency;' +
+    'SumOfItems: Currency Calc(Items);' +
+    'TotalBudget: Currency Calc(SumOfItems, Shipping);' +
+    'Items: Parts(TBudgetItem);' +
+    ');';
+end;
 
 { TProduct }
 
 class function TProduct.InternalMetadataStr: string;
 begin
   Result := 'TProduct IsPersistent(' +
-    'Code: PlainString;' +
-    'Name: AnsiString DisplayName="Name" IsMandatory=True;' +
+    'Code: PlainString(20);' +
+    'Name: AnsiString(40) DisplayName="Name" IsMandatory=True;' +
     'Remarks: Memo' +
     'Unity: Reference(TUnity);' +
     'MinimumStock: Float;' +
@@ -50,9 +162,17 @@ end;
 
 initialization
   TProduct.RegisterClass;
+  TBudget.RegisterClass;
+  TBudgetItem.RegisterClass;
+  TInvoice.RegisterClass;
+  TInvoiceItem.RegisterClass;
 
 finalization
   TProduct.UnregisterClass;
+  TBudget.UnregisterClass;
+  TBudgetItem.UnregisterClass;
+  TInvoice.RegisterClass;
+  TInvoiceItem.UnregisterClass;
 
 end.
 
